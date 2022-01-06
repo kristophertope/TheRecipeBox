5b266a8c38988c03163bbfcc65aa7cb9f919a130
kristophertope <78042859+kristophertope@users.noreply.github.com>
2022-01-06 08:53:36 -0600

    Initial commit

---
+++ recipes.js
@@ -0,0 +1,40 @@
+let recipe0 = {
+    name : "Crockpot Chili",
+    image : "chili.jpg",
+    servings : 6,
+    prepTime : "25 minutes",
+    cookTime : "6 hours",
+    difficulty : 2,
+    ingredientQuantities : [2, 1, 1, 4, 1, 1, 2, 2, 2, 2, 1, .5],
+    ingredientUnits : ["tbsp", "cup", "cup", "tbsp", "tsp", "lb", "cans", "cans", "cans", "cans", "cup", "cup"],
+    ingredientNames : ["cooking oil", "onion", "chopped peppers", "chilli powder", "hot chilli powder (optional)", "ground beef or chicken", "red beans", "kidney beans", "tomato puree", "tomato sauce", "shredded cheese (optional)", "sour cream"],
+    instructions : ["Heat cooking oil in 2 quart skillet.", "Saute onions and peppers for 5 minutes.", "Add spices and stir for 30 seconds.", "Add meat and cook until browned. Approximately 15 minutes.", "Pour contents of skillet into 3 quart crock pot.", "Rinse beans and place in crockpot.", "Open and pour Tomato puree and sauce into crock pot.", "Cover crockpot and cook on low for 6 hours.", "Serve into individual bowls and top with sour cream and cheese."]
+}
+
+let recipe1 = {
+    name : "Tamale Pie",
+    image : "tamale-pie.jpg",
+    servings : 6,
+    prepTime : "10 minutes",
+    cookTime : "1 hours",
+    difficulty : 3,
+    ingredientQuantities : [1, 1.5, 1, 1, .5, 2, 2, 14.5, 4, 16, 4, .5, 1, .3, 16],
+    ingredientUnits : ["tbsp", "lbs", "onion", "green bell pepper", "tsp", "tsp", "tsp", "ounce", "ounce", "ounce", "ounce", "cup", "cup", "cup", "ounce"],
+    ingredientNames : ["extra virgin olive oil", "lean ground beef", "chopped", "chopped", "salt", "chili powder", "cumin", "fire-roasted tomatoes", "green Anaheim chilies", "whole kernel corn", "ripe olives", "raisins", "grated sharp cheddar cheese", "water", "prepared cornbread mix"],
+    instructions : ["Preheat oven to 375°F", "Brown the beef, onions, and bell pepper", "Mix in fire-roasted tomatoes, Anaheim chiles, corn, olives, raisins, cheese, and water. Adjust seasoning. Add more chile powder and cumin if desired", "Pour the filling into the casserole", "In a large bowl, prepare the cornbread batter according to the directions on the package. Pour the cornbread batter over the top of the filling in the casserole", "Bake at 375°F for 40 minutes, until the top is browned"]
+}
+
+let recipe2 = {
+    name : "Baked Eggs in Avocado with Salmon",
+    image : "avocado-eggs.jpg",
+    servings : 6,
+    prepTime : "10 minutes",
+    cookTime : "20 minutes",
+    difficulty : 1,
+    ingredientQuantities : [3, 6, 6, .5, .5, .25, .3],
+    ingredientUnits : ["large Haas avocados", "ounces", "large eggs", "tsp", "tsp", "tsp", "cup"],
+    ingredientNames : ["halved", "smoked salmon, thinly sliced", "", "salt", "freshly ground black pepper", "red chili pepper flakes", "sliced red onion"],
+    instructions : ["Preheat the oven to 425°F", "Using a spoon, scoop out about one tablespoon of avocado flesh, or more, as needed to enlarge the well in the center of each avocado", "In a small bowl, combine the salt, freshly ground black pepper, and red pepper flakes. Into a separate small bowl, gently crack 1 egg and season with a pinch or two of the salt, pepper, and red chili pepper flake mixture", "Use a spoon to carefully transfer the seasoned yolk into the avocado well trying to keep the yolk intact. If there are any remaining egg whites, add them into the avocado well. Repeat the process with remaining eggs until all of the avocado halves have been filled", "Add toppings and bake the avocados for 15-20 minutes until the egg whites have set", "Garnish and serve"]
+}
+
+let siteRecipes = [recipe0, recipe1, recipe2];
